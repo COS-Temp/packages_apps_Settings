82d263f42cce364be684aa14159592f311465df1
Adin Kwok <adin.kwok@carbonrom.org>
2018-08-11 18:07:14 -0700

    Settings: External Smart Pixels fragment launch
    Necessary for Smart Pixels tile.
    Based on change by Mazda--.
    
    Reference commit:
    https://github.com/DirtyUnicorns/commit/c637500b89bfe1b726a92efa358cf48b1a2a3093
    
    Change-Id: I5a1892d8211ac968958395326fbbbfcdfcf7e942
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -139,6 +139,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.cosmic.settings.Galaxy;
+import com.cosmic.settings.display.SmartPixels;
 
 public class SettingsGateway {
 
@@ -266,6 +267,7 @@ public class SettingsGateway {
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             Galaxy.class.getName(),
+            SmartPixels.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -305,5 +307,6 @@ public class SettingsGateway {
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.GalaxyActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName(),
     };
 }
